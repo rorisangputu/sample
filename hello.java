4985cc39553b0b9e0c0b5b4f76784876e0866d35
Rorisang Putu <rorisangputu@gmail.com>
2024-12-20 13:16:51 +0200

    relearing java

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("The sum is" + ": " + Sum(5, 10));
+    }
+
+    public static int Sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+}
